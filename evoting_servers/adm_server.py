93cecd2695666d3331a55f9386665f4ecea9da36
VitorRez <vitorez2002@gmail.com>
2024-12-12 10:28:12 -0300

    commit de teste

--- evoting_servers/adm_server.py
+++ evoting_servers/adm_server.py
@@ -78,6 +78,7 @@ class administrator_server():
 
     def get_msg(self, conn):
         connected = True
+        
         while connected:
             msg_length = conn.recv(HEADER).decode(FORMAT)
             if msg_length:
